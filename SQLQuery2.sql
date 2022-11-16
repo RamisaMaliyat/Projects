dff1b11883ed38ea139cb037ea543656b99e1a72
Ramisa Maliyat <49657454+RamisaMaliyat@users.noreply.github.com>
2022-11-16 23:29:52 +0600

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[File] (
-    [FileId]    INT             IDENTITY (1, 1) NOT NULL,
-    [FileName]  NVARCHAR (100)  NULL,
-    [Filebytes] VARBINARY (MAX) NULL,
-    CONSTRAINT [PK_dbo.File] PRIMARY KEY CLUSTERED ([FileId] ASC)
-);
