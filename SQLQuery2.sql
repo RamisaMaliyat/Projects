d823c779a66bdab65dd6e1e413b1217acfb9a30d
Ramisa Maliyat <49657454+RamisaMaliyat@users.noreply.github.com>
2022-11-16 23:26:29 +0600

    Pair Up

---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[File] (
+    [FileId]    INT             IDENTITY (1, 1) NOT NULL,
+    [FileName]  NVARCHAR (100)  NULL,
+    [Filebytes] VARBINARY (MAX) NULL,
+    CONSTRAINT [PK_dbo.File] PRIMARY KEY CLUSTERED ([FileId] ASC)
+);
